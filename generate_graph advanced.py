09db7b9b18390988a3a3b61fbf37884bc0d10763
tomekrzymyszkiewicz <tomek.rzymyszkiewicz@gmail.com>
2021-05-28 19:44:13 +0200

    adding weights modified

--- generate_graph advanced.py
+++ generate_graph advanced.py
@@ -4,6 +4,7 @@ from itertools import combinations
 from random import random
 import matplotlib.pyplot as plt
 import networkx as nx
+from numpy.core.fromnumeric import size
 
 
 def main():
@@ -15,10 +16,14 @@ def main():
     min_weight = int(config['graph']['min_weight'])
     max_weight = int(config['graph']['max_weight'])
     G = nx.fast_gnp_random_graph(number_of_vertices, density)
+    weights = np.random.randint(min_weight, max_weight,G.number_of_edges())
+    graph_edges_list = list()
+    for iteration, edge in enumerate(G.edges()):
+        graph_edges_list.append([edge[0],edge[1],weights[iteration]])
+    print(graph_edges_list)
     with open(graph_name +"-"+ str(G.number_of_nodes()) + "vertices" + "-" + str(G.number_of_edges())+"edges-"+config['graph']['density']+"density.csv", mode='w') as file:
-        for edge in G.edges():
-            file.write(" ".join([str(edge[0]), str(edge[1]), str(
-                np.random.randint(min_weight, max_weight))]))
+        for edge in graph_edges_list:
+            file.write(" ".join([str(edge[0]), str(edge[1]), str(edge[2])]))
             file.write('\n')
     nx.draw(G)
     plt.show()
