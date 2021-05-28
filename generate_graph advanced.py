1ea1174ba815436fce79548d6f18f146b1bc813c
tomekrzymyszkiewicz <tomek.rzymyszkiewicz@gmail.com>
2021-05-28 18:54:54 +0200

    graph file naming extended

--- generate_graph advanced.py
+++ generate_graph advanced.py
@@ -5,18 +5,20 @@ from random import random
 import matplotlib.pyplot as plt
 import networkx as nx
 
+
 def main():
     config = configparser.ConfigParser()
     config.read('adv-data-config.ini')
-    file_name = config['graph']['file_name']
+    graph_name = config['graph']['graph_name']
     number_of_vertices = int(config['graph']['vertices'])
     density = float(config['graph']['density'])
     min_weight = int(config['graph']['min_weight'])
     max_weight = int(config['graph']['max_weight'])
-    G = nx.fast_gnp_random_graph(number_of_vertices,density)
-    with open(file_name[0:-4] + "-"+ str(G.number_of_edges())+"edges.csv", mode='w') as file:
+    G = nx.fast_gnp_random_graph(number_of_vertices, density)
+    with open(graph_name +"-"+ str(G.number_of_nodes()) + "vertices" + "-" + str(G.number_of_edges())+"edges-"+config['graph']['density']+"density.csv", mode='w') as file:
         for edge in G.edges():
-            file.write(" ".join([str(edge[0]),str(edge[1]),str(np.random.randint(min_weight,max_weight))]))
+            file.write(" ".join([str(edge[0]), str(edge[1]), str(
+                np.random.randint(min_weight, max_weight))]))
             file.write('\n')
     nx.draw(G)
     plt.show()
