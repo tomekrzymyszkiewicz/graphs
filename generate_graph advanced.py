96d395e7aa4e55b154cdbbea7bc64b820da399f0
tomekrzymyszkiewicz <tomek.rzymyszkiewicz@gmail.com>
2021-05-28 17:11:52 +0200

    generating grahps by num of vertices and density

---
+++ generate_graph advanced.py
@@ -0,0 +1,25 @@
+import numpy as np
+import configparser
+from itertools import combinations
+from random import random
+import matplotlib.pyplot as plt
+import networkx as nx
+
+def main():
+    config = configparser.ConfigParser()
+    config.read('adv-data-config.ini')
+    file_name = config['graph']['file_name']
+    number_of_vertices = int(config['graph']['vertices'])
+    density = float(config['graph']['density'])
+    min_weight = int(config['graph']['min_weight'])
+    max_weight = int(config['graph']['max_weight'])
+    G = nx.fast_gnp_random_graph(number_of_vertices,density)
+    with open(file_name, mode='w') as file:
+        for edge in G.edges():
+            file.write(" ".join([str(edge[0]),str(edge[1]),str(np.random.randint(min_weight,max_weight))]))
+            file.write('\n')
+    nx.draw(G)
+    plt.show()
+
+
+main()
